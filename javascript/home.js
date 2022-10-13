e7aa8795ace44d436bf250ccd20a527332c85ff6
ssamu801 <114760962+ssamu801@users.noreply.github.com>
2022-10-14 02:09:02 +0800

    Add files via upload

---
+++ javascript/home.js
@@ -0,0 +1,20 @@
+function toSTProf() {
+    window.location.href="prof_listST.html";
+    }
+  
+  function toCTProf() {
+      window.location.href="prof_listCT.html";
+  }
+  
+  function toITProf() {
+    window.location.href="prof_listIT.html";
+  }
+  
+  function toBSProf() {
+    window.location.href="prof_listBS.html";
+  }
+  
+  function toCommProf() {
+    window.location.href="prof_listComm.html";
+  }
+  
